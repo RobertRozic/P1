203b1ca0c4bdcc7e4952bef7339aa8a36a485863
RobertRozic <robert.rozic@gmail.com>
2022-12-15 00:28:41 +0100

    Vjezbe pt.1

---
+++ hello_world.py
@@ -0,0 +1,37 @@
+#print("Hello world")
+#print(1)
+#print("Hello world", 1)
+#print(1, 2, 3, sep="*")
+
+#a = input("Unesite vase ime:")
+#print("Vase ime je:", a)
+
+# Ovo je program za zbroj dva broja
+'''a = int(input("Unesite prvi broj"))
+b = int(input("Unesiite drugi broj"))
+
+c = a+b
+print(c)
+'''
+# Zbroj dva broja spremamo u varijablu a
+a = 2 + 2
+
+# Ispisujemo varijablu a, odnosno zbroj
+print(a)
+
+"""
+ Zatrazi od korisnika unos 2 broja...
+ Viselinijski
+ komentar
+"""
+
+a = True
+print(a, "je tip", type(a))
+
+br_int = 123
+br_int = str(br_int)
+
+br_str = "456"
+#br_str = int(br_str)
+
+print(br_int + br_str)
---
+++ razgranata.py
@@ -0,0 +1,53 @@
+# Ispiši poruku ako
+# je uneseni broj pozitivan
+'''broj = int(input("Unesi broj:"))
+
+if broj > 0:
+    print("Broj je pozitivan!")
+'''
+'''
+ Ispiši poruku ako je
+ uneseni broj pozitivan ili 0,
+ inače ispisi da je negativan.
+'''
+
+'''
+broj = int(input("Unesi broj"))
+
+if broj >= 0:
+    print("Broj je pozitivan ili 0!")
+else:
+    print("Broj je negativan")
+
+
+broj = int(input("Unesi broj:"))
+
+if broj == 0:
+    print("Broj je 0!")
+elif broj > 0:
+    print("Broj je pozitivan!")
+else:
+    print("Broj je negativan!")
+    print("Test")
+'''
+# Nested
+broj = int(input("Unesi broj:"))
+
+if broj >= 0:
+    if broj == 0:
+        print("NULA!")
+    else:
+        if broj > 5:
+            print("Broj je veci od 5")
+        else:
+            print("Pozitivan")
+ else:
+    print("Negativan broj!")
+
+
+
+
+
+
+
+
