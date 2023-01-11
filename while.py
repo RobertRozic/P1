5ce64f5592ae9494a150abedec48cfece4b928ca
RobertRozic <robert.rozic@gmail.com>
2023-01-11 13:41:48 +0100

    Vjezbe 11.1.2023.

---
+++ while.py
@@ -0,0 +1,21 @@
+a = 1
+
+#while a <= 5:
+#    print(a)
+#    a = a + 1 # a == 2, a == 3,  
+
+suma = 0
+for i in range(10):
+    suma += i
+
+print("Zbroj je", suma)
+
+suma = 0
+n = 10
+i = 0
+
+while i < 10:
+    suma += i
+    i += 1
+
+print("Zbroj je", suma)
