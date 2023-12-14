3abcf932936eefaaa2561a6b10ae6c2af61de406
RobertRozic <r.rozic97@gmail.com>
2023-12-14 14:54:58 +0100

    Clean for 23/24

--- 06.Priprema za 1.kol/zad1.py
+++
@@ -1,6 +0,0 @@
-# Napiši program u kojem ćeš izračunati1
-# i ispisati površinu kruga radijusa 5cm.
-r = int(input("Unesi radijus kruga"))
-pi = 3.14
-p = r**2 * pi
-print("Povrsina kruga je", p, "cm2")
