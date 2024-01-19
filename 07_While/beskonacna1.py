094ee5d392ee9a627e15d76146298b34f064db1d
RobertRozic <r.rozic97@gmail.com>
2024-01-19 14:09:32 +0100

    Beskonacna petlja

---
+++ 07_While/beskonacna1.py
@@ -0,0 +1,9 @@
+#while True:
+#    print("Pozdrav!")
+
+# Program koji od korisnika trazi unos parnog broja
+# ponavljaj upit sve dok korisnik ne unese paran broj
+while 1: # while True
+    unos = int(input("Unesi paran broj:"))
+    if unos % 2 == 0: # broj je paran
+        break
