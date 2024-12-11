b364dfc5cd4dac7181232940c29c0fa9ce933551
Robert Rozic <r.rozic97@gmail.com>
2024-12-11 15:30:31 +0100

    Vjezbe 11.12.2024.

---
+++ 05_Kontrola_toka/05_liste.py
@@ -0,0 +1,13 @@
+brojevi = [1, 3, 5, 10] # definiranje liste
+print(brojevi) # ispis liste
+print(type(brojevi)) # ispis tipa varijable brojevi
+
+for element in brojevi:
+    print(element)
+
+tekst = "programiranje"
+
+for slovo in tekst:
+    print(slovo, end="#\n")
+
+
---
+++ 05_Kontrola_toka/05_zbroj.py
@@ -0,0 +1,12 @@
+# Zbroj parnih brojeva od 100 do 1000
+zbroj = 0
+
+brojevi = range(100, 1000, 2)
+
+for i in brojevi:
+    zbroj += i
+
+print("Zbroj je:", zbroj)
+
+
+
