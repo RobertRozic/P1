1f5c751eb9791bb76800b5fc5d1255f90d6ff4d2
Robert Rozic <r.rozic97@gmail.com>
2025-01-08 14:44:57 +0100

    Vjezbe 8.1.2025.

---
+++ 07_While/zad4.py
@@ -0,0 +1,45 @@
+brojevi = [1, 1, 2, 3, 5, 8, 13]
+
+tekst = "danas"
+# ili
+#lista = ["d", "a", "n", "a", "s"]
+
+# Zbrajanje
+print(tekst + "sutra")
+print(brojevi + [1, 2])
+
+# Mnozenje
+print("danas" * 3)
+print(brojevi * 3)
+
+# Indeksiranje
+print(tekst[2])
+
+# Podniz
+print(tekst[0:3])
+print(tekst[2:5])
+
+# Provjera je li string sadrzan u drugom stringu
+print("dan" in "sutra")
+print("dan" in "danas")
+print("dan" not in "danas")
+
+# Indeks sa zada
+print(tekst[-1])
+# svako drugo slovo
+print(tekst[::2])
+
+# tekst unazad
+print(tekst[::-1])
+print(tekst[::-2])
+
+
+
+
+
+
+
+
+
+
+
