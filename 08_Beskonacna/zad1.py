a95f32c22707d68722b61c59b56c2bbb655c9702
Robert Rozic <r.rozic97@gmail.com>
2025-01-27 13:51:06 +0100

    Vjezbe 27.1.2025.

---
+++ 08_Beskonacna/zad1.py
@@ -0,0 +1,5 @@
+while True:
+    print('Pozdrav!')
+    x = int(input("Unesi paran broj: "))
+    if x % 2 == 0:
+        break
---
+++ 09_Funkcije/bib1.py
@@ -0,0 +1,11 @@
+x = 16
+
+import math
+print("Korijen iz 16:", math.sqrt(x))
+
+from math import sqrt
+print("Korijen iz 16:", sqrt(x))
+
+print("Pi je:", math.pi)
+
+
