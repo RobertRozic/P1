d59f4853771a2e1603671bdd5e2eed9228b1d7e1
Robert Rozic <r.rozic97@gmail.com>
2025-02-05 14:33:40 +0100

    Vjezba

---
+++ Rijeseni_zadaci/kol2/primjer_ispita.py
@@ -0,0 +1,26 @@
+# Primjer ispita
+
+'''
+Zatražiti od korisnika unos imena i prezimena u dvije razlicite varijable.
+Ispisati prezime onoliko puta koliko se samoglasnika pojavljuje u imenu.
+Npr. za unos Petar Petric prezime ce se ispisati 2 puta.
+Prezime neka bude ispisano velikim slovima.
+'''
+
+'''
+Koristeći while petlju zatražiti od korisnika unos 10 troznamenkastih parnih brojeva.
+Prebrojati koliko puta je korisnik unio broj manji ili jednak 500, dok je unosio 10 traženih brojeva.
+Prebrojati koliko puta se u 10 ispravno unesenih brojeva pojavila znamenka 4.
+'''
+
+'''
+Koristeći while petlju i break iskaz od korisnika tražiti unos artikala u kasu.
+Naziv artikla, kolicinu i cijenu spremati u odvojene liste.
+Za dodavanje artikla koristiti funkciju koja kao parametre prima naziv, kolicinu i cijenu i ne vraća ništa.
+Korisnik unosi artikle sve dok ne unese 0 ili negativan broj.
+
+Program nije moguće zaustaviti sve dok nisu unesena barem 3 ispravna artikla.
+Ime artikla mora biti duže od 3 slova a cijena i stanje pozitivni kako bi se pozvala funkcija za spremanje.
+
+Na kraju programa ispisati koliko je uneseno artikala.
+'''
